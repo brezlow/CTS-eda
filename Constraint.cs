eb542333bd8580a0f3e5620eb06f854ae8603516
brezlow <joyfulcho@foxmail.com>
2024-11-05 08:24:12 +0000

    fix

--- Constraint.cs
+++
@@ -1,7 +0,0 @@
-﻿class Constraint
-{
-    public int MaxFanout { get; set; }
-    public double MaxRC { get; set; }
-    public double BufferDelay { get; set; }
-    public (double Resistance, double Capacitance) UnitRC { get; set; }
-}
